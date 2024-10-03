11f401519e595912956d70c9088130e6f01c9a47
shadow6444 <shadowgarden644@gmail.com>
2024-10-03 16:05:49 +0500

    Create Home.jsx

---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import "./Home.css";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <main>
+      <div className="home-hero-img">
+        <div className="home-hero-header">
+          <h1 className="home-hero-text">
+            You got the travel plans, we got the travel vans.
+          </h1>
+          <div className="home-hero-info">
+            <p>
+              Add adventure to your life by joining the #vanlife movement. Rent
+              the perfect van to make your perfect road trip.
+            </p>
+            <Link className="home-link-button" to="/vans">
+              Find your van
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
