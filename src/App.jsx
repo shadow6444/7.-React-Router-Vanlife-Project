99bc83ce16d9693a1ec67accd5844e26a312e5fa
shadow6444 <shadowgarden644@gmail.com>
2024-10-04 22:28:18 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
 import "./App.css";
-import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import About from "./components/pages/About";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import MainLayout from "./components/Layout/MainLayout";
 import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import VanList from "./components/pages/Vans/VanList";
+import VanDetail from "./components/pages/Vans/VanDetail";
+import Dashboard from "./components/pages/Host/Dashboard";
+import Income from "./components/pages/Host/Income";
+import Review from "./components/pages/Host/Review";
+
+import "./server";
+import HostLayout from "./components/Layout/HostLayout";
+
 function App() {
   return (
     <Router>
-      <header className="page-header">
-        <Link className="header-logo" to="/">
-          #VANLIFE
-        </Link>
-        <nav className="navbar">
-          <Link className="nav-links" to="/about">
-            About
-          </Link>
-          <Link className="nav-links" to="./vans">
-            Vans
-          </Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<VanList />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Review />} />
+          </Route>
+        </Route>
       </Routes>
-      <footer className="page-footer">
-        <p>Ⓒ 2022 #VANLIFE</p>
-      </footer>
     </Router>
   );
 }
