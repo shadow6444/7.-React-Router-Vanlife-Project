392a3ec86d184d1888ad79c1bc4611647291825b
shadow6444 <shadowgarden644@gmail.com>
2024-10-04 22:28:58 +0500

    Create MainLayout.jsx

---
+++ src/components/Layout/MainLayout/MainLayout.jsx
@@ -0,0 +1,17 @@
+import './MainLayout.css'
+import { Outlet } from "react-router-dom";
+import Header from './Header';
+import Footer from "./Footer";
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
