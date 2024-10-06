f4916058836686ac9e61871d261cdc6505c397bc
shadow6444 <shadowgarden644@gmail.com>
2024-10-06 09:38:32 +0500

    Update HostLayout.jsx

--- src/components/Layout/HostLayout/HostLayout.jsx
+++ src/components/Layout/HostLayout/HostLayout.jsx
@@ -1,11 +1,43 @@
-import { Outlet } from "react-router-dom";
 import "./HostLayout.css";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const HostLayout = () => {
+  const isActiveStyles = {
+    fontWeight: "700",
+    color: "#161616",
+    textDecoration: "underline",
+  };
+
   return (
     <>
-      <h1>Host Layout goes here!</h1>
-      <Outlet/>
+      <div className="host-layout">
+        <NavLink
+          style={({ isActive }) => (isActive ? isActiveStyles : null)}
+          end
+          to="."
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? isActiveStyles : null)}
+          to="income"
+        >
+          Income
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? isActiveStyles : null)}
+          to="vans"
+        >
+          Vans
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? isActiveStyles : null)}
+          to="reviews"
+        >
+          Reviews
+        </NavLink>
+      </div>
+      <Outlet />
     </>
   );
 };
