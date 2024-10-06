ab453925cd0dbb50c5eed63d7ceb23a3a1d8eb4e
shadow6444 <shadowgarden644@gmail.com>
2024-10-06 15:29:31 +0500

    Update Header.jsx

--- src/components/Layout/MainLayout/Header/Header.jsx
+++ src/components/Layout/MainLayout/Header/Header.jsx
@@ -1,7 +1,12 @@
-import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 
+import { Link, NavLink } from "react-router-dom";
+import { RxAvatar } from "react-icons/rx";
+
 const Header = () => {
+  function fakeLogOut() {
+    localStorage.removeItem("loggedin");
+  }
   return (
     <header className="page-header">
       <Link className="header-logo" to="/">
@@ -10,22 +15,26 @@ const Header = () => {
       <nav className="navbar">
         <NavLink
           className={({ isActive }) => (isActive ? "active" : null)}
-          to="/host"
+          to="host"
         >
           Host
         </NavLink>
         <NavLink
           className={({ isActive }) => (isActive ? "active" : null)}
-          to="/about"
+          to="about"
         >
           About
         </NavLink>
         <NavLink
           className={({ isActive }) => (isActive ? "active" : null)}
-          to="./vans"
+          to="vans"
         >
           Vans
         </NavLink>
+        <NavLink to="login">
+          <RxAvatar className="login-link" />
+        </NavLink>
+        <button onClick={fakeLogOut}>X</button>
       </nav>
     </header>
   );
