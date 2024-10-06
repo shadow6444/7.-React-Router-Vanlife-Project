c0adaf5b44c39f856cf57fbb68195aa73c815f02
shadow6444 <shadowgarden644@gmail.com>
2024-10-06 15:30:37 +0500

    Update VanDetail.jsx

--- src/components/pages/Vans/VanDetail/VanDetail.jsx
+++ src/components/pages/Vans/VanDetail/VanDetail.jsx
@@ -2,6 +2,7 @@ import "./VanDetail.css";
 import { Link, useLocation, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { FaArrowLeft } from "react-icons/fa6";
+import { getVanDetail } from "../../../../api";
 const VanDetail = () => {
   const [isFetching, setIsFetching] = useState(false);
   const [error, setError] = useState();
@@ -14,15 +15,10 @@ const VanDetail = () => {
       setIsFetching(true);
 
       try {
-        const response = await fetch(`/api/vans/${params.id}`);
-        const resData = await response.json();
-
-        if (!response.ok) {
-          throw new Error("Failed to Fetch Data!");
-        }
-        setVan(resData.vans);
+        const vanDetails = await getVanDetail(params.id);
+        setVan(vanDetails);
       } catch (error) {
-        setError({ message: error.message || "Could not fetch data!" });
+        setError(error);
       }
       setIsFetching(false);
     }
@@ -32,35 +28,40 @@ const VanDetail = () => {
   const search = state?.search || "";
   const type = state?.type || "all";
 
-  return isFetching ? (
-    <p>Fetching Data From API...</p>
-  ) : error ? (
-    <p>{error.message}</p>
-  ) : (
+  return (
     <section className="van-detail-container">
-      <Link to={`..${search}`} relative="path" className="back-link">
-        <FaArrowLeft />
-        <p>Back to {type} vans</p>
-      </Link>
-
-      {/* <Link to={`..${search}`} relative="path" className="back-link">
-        <FaArrowLeft />
-        <p>Back to {search === "?" ? "all vans" : `${search.slice(6)} vans`}</p>
-      </Link> */}
-
-      <div className="van-details">
-        <img src={van.imageUrl} alt="van image" />
-        <h3 className={van.type}>{van.type}</h3>
-        <h1>{van.name}</h1>
-        <h2>
-          ${van.price}
-          <span>/day</span>
-        </h2>
-        <p>{van.description}</p>
-        <Link>Rent this van</Link>
-      </div>
+      {isFetching ? (
+        <h2 className="fetch-api">Fetching van details from API...</h2>
+      ) : error ? (
+        <h2 className="error-api">{error.message}</h2>
+      ) : (
+        <>
+          <Link to={`..${search}`} relative="path" className="back-link">
+            <FaArrowLeft />
+            <p>Back to {type} vans</p>
+          </Link>
+          <div className="van-details">
+            <img src={van.imageUrl} alt="van image" />
+            <h3 className={van.type}>{van.type}</h3>
+            <h1>{van.name}</h1>
+            <h2>
+              ${van.price}
+              <span>/day</span>
+            </h2>
+            <p>{van.description}</p>
+            <Link>Rent this van</Link>
+          </div>
+        </>
+      )}
     </section>
   );
 };
 
 export default VanDetail;
+
+{
+  /* <Link to={`..${search}`} relative="path" className="back-link">
+        <FaArrowLeft />
+        <p>Back to {search === "?" ? "all vans" : `${search.slice(6)} vans`}</p>
+      </Link> */
+}
