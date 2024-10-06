c3c9397236964d676cfbaf8e0fb417d893071109
shadow6444 <shadowgarden644@gmail.com>
2024-10-06 09:40:46 +0500

    Added useLocation

--- src/components/pages/Vans/VanDetail/VanDetail.jsx
+++ src/components/pages/Vans/VanDetail/VanDetail.jsx
@@ -1,14 +1,18 @@
 import "./VanDetail.css";
-import { Link, useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { FaArrowLeft } from "react-icons/fa6";
 const VanDetail = () => {
+  const [isFetching, setIsFetching] = useState(false);
   const [error, setError] = useState();
   const [van, setVan] = useState([]);
   const params = useParams();
+  const { state } = useLocation();
 
   useEffect(() => {
     async function fetchData() {
+      setIsFetching(true);
+
       try {
         const response = await fetch(`/api/vans/${params.id}`);
         const resData = await response.json();
@@ -16,21 +20,34 @@ const VanDetail = () => {
         if (!response.ok) {
           throw new Error("Failed to Fetch Data!");
         }
-        console.log(resData.vans);
         setVan(resData.vans);
       } catch (error) {
         setError({ message: error.message || "Could not fetch data!" });
       }
+      setIsFetching(false);
     }
     fetchData();
   }, []);
 
-  return van ? (
+  const search = state?.search || "";
+  const type = state?.type || "all";
+
+  return isFetching ? (
+    <p>Fetching Data From API...</p>
+  ) : error ? (
+    <p>{error.message}</p>
+  ) : (
     <section className="van-detail-container">
-      <Link to="/vans" className="back-link">
+      <Link to={`..${search}`} relative="path" className="back-link">
         <FaArrowLeft />
-        <p>Back to all vans</p>
+        <p>Back to {type} vans</p>
       </Link>
+
+      {/* <Link to={`..${search}`} relative="path" className="back-link">
+        <FaArrowLeft />
+        <p>Back to {search === "?" ? "all vans" : `${search.slice(6)} vans`}</p>
+      </Link> */}
+
       <div className="van-details">
         <img src={van.imageUrl} alt="van image" />
         <h3 className={van.type}>{van.type}</h3>
@@ -43,8 +60,6 @@ const VanDetail = () => {
         <Link>Rent this van</Link>
       </div>
     </section>
-  ) : (
-    <h1>Loading Van Details...</h1>
   );
 };
 
